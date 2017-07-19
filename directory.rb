cea2b4074a890f3140cc0005a9d66663efc19e7e
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-07-19 14:28:47 +0100

    Interactive student list

--- directory.rb
+++ directory.rb
@@ -1,16 +1,31 @@
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :November},
-{name: "Darth Vader", cohort: :November},
-{name: "Nurse Ratched", cohort: :November},
-{name: "Michael Corleone", cohort: :November},
-{name: "Alex DeLarge", cohort: :November},
-{name: "The Wicked Witch of the West", cohort: :November},
-{name: "Terminator", cohort: :November},
-{name: "Freddy Krueger", cohort: :November},
-{name: "The Joker", cohort: :November},
-{name: "Joffrey Baratheon", cohort: :November},
-{name: "Norman Bates", cohort: :November}
-] # <--- List of the students. ~
+# students = [
+# {name: "Dr. Hannibal Lecter", cohort: :November},
+# {name: "Darth Vader", cohort: :November},
+# {name: "Nurse Ratched", cohort: :November},
+# {name: "Michael Corleone", cohort: :November},
+# {name: "Alex DeLarge", cohort: :November},
+# {name: "The Wicked Witch of the West", cohort: :November},
+# {name: "Terminator", cohort: :November},
+# {name: "Freddy Krueger", cohort: :November},
+# {name: "The Joker", cohort: :November},
+# {name: "Joffrey Baratheon", cohort: :November},
+# {name: "Norman Bates", cohort: :November}
+# ] # <--- List of the students. ~
+
+
+def input_students
+	puts "Please enter the names of the students."
+	puts "To finish, just hit the return button twice."
+	students = []
+	name = gets.chomp
+	while !name.empty? do
+		students << {name: name, cohort: :November}
+		puts "#{name} has been added! Now we have #{students.count} students."
+		name = gets.chomp
+	end
+	students
+end
+
 
 def header
 	puts "The students of Villains Academy"
@@ -26,6 +41,8 @@ def footer(names)
 	puts "Overall, we have #{names.count} great students."
 end # <--- Interpolation to count the students || footer. ~
 
+
+students = input_students
 header # <--- Calling header method. ~
 student_list(students) # <--- Calling student_list method. ~
 footer(students) # <--- Calling footer method. ~
