7e54a63bb82aee7b21bf397fd7140b45a7ece31c
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-07-19 10:28:12 +0100

    directory.rb 4.0

--- directory.rb
+++ directory.rb
@@ -12,8 +12,20 @@ students = [
 "Norman Bates"
 ] # <--- List of the students. ~
 
-puts "The students of Villains Academy"
-puts "------------"
-students.each { |student| puts student } # <--- Iteration of the array. ~
-puts "------------"
-puts "Overall, we have #{students.count} great students." # <--- Interpolation to count the students. ~
+def header
+	puts "The students of Villains Academy"
+	puts "------------"	
+end # <--- Header. ~
+
+def student_list(names)
+	names.each { |name| puts name }
+end # <--- Iteration of the array. ~
+
+def footer(names)
+	puts "------------"
+	puts "Overall, we have #{names.count} great students."
+end # <--- Interpolation to count the students || footer. ~
+
+header # <--- Calling header method. ~
+student_list(students) # <--- Calling student_list method. ~
+footer(students) # <--- Calling footer method. ~
