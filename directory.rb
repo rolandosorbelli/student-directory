15f6e3d2336e4a743d391de2ec0010ee317b8886
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-07-20 18:06:43 +0100

    students.csv created

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
 def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
+	puts "3. Save the list to students.csv"
 	puts "9. Exit"
 end
 
@@ -19,6 +20,8 @@ def process(selection)
 			input_students
 		when "2"
 			show_students
+		when "3"
+			save_students
 		when "9"
 			exit
 		else
@@ -53,6 +56,17 @@ def student_list
 		puts "- #{student[:name]}, #{student[:cohort]} cohort."
 	end
 end # <--- Iteration of the array. ~
+
+def save_students
+	file = File.open("students.csv", "w")
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
+	end
+	file.close
+	puts "The list of students has been saved!"
+end
 
 def footer
 	puts "~~~~~~~~~~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"
