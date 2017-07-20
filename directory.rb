098594fb7dcbdfca2ea90d2fcd6f560338ef6e73
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-07-20 17:12:46 +0100

    more refactoring

--- directory.rb
+++ directory.rb
@@ -1,51 +1,63 @@
-# students = [
-# {name: "Dr. Hannibal Lecter", cohort: :November},
-# {name: "Darth Vader", cohort: :November},
-# {name: "Nurse Ratched", cohort: :November},
-# {name: "Michael Corleone", cohort: :November},
-# {name: "Alex DeLarge", cohort: :November},
-# {name: "The Wicked Witch of the West", cohort: :November},
-# {name: "Terminator", cohort: :November},
-# {name: "Freddy Krueger", cohort: :November},
-# {name: "The Joker", cohort: :November},
-# {name: "Joffrey Baratheon", cohort: :November},
-# {name: "Norman Bates", cohort: :November}
-# ] # <--- List of the students. ~
+@students = []
 
+def interactive_menu
+	loop do
+		print_menu
+		process(gets.chomp)
+	end
+end
+
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end
+
+def process(selection)
+	case selection
+		when "1"
+			input_students
+		when "2"
+			show_students
+		when "9"
+			exit
+		else
+			puts "I don't know what you meant, try again."
+	end
+end
 
 def input_students
 	puts "Please enter the names of the students."
 	puts "To finish, just hit the return button twice."
-	students = []
 	name = gets.chomp
 	while !name.empty? do
-		students << {name: name, cohort: :November}
-		puts "#{name} has been added! Now we have #{students.count} students."
+		@students << {name: name, cohort: :November}
+		puts "#{name} has been added! Now we have #{@students.count} students."
 		name = gets.chomp
 	end
-	students
-end 
+end
 
+def show_students
+	header
+	student_list
+	footer
+end
 
 def header
 	puts "The students of Villains Academy"
 	puts "~~~~~~~~~~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"	
 end # <--- Header. ~
 
-def student_list(students)
-	students.each_with_index do
-		|student|
+def student_list
+	@students.each_with_index do |student|
 		puts "- #{student[:name]}, #{student[:cohort]} cohort."
 	end
 end # <--- Iteration of the array. ~
 
-def footer(names)
+def footer
 	puts "~~~~~~~~~~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"
-	puts "Overall, we have #{names.count} great students."
+	puts "Overall, we have #{@students.count} great students."
 end # <--- Interpolation to count the students || footer. ~
 
 
-students = input_students
-header # <--- Calling header method. ~
-student_list(students) # <--- Calling student_list method. ~
-footer(students) # <--- Calling footer method. ~
+interactive_menu
